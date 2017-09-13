96349d4520d2653f739ec7a98d3e39015a725285
Alexander Gehrke <github@qwertyuiop.de>
2017-09-13 18:28:47 +0200

    Migrate tapestry to external repo

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # awesome-tapestry
-awesome wallpaper manager
+
+Command line tool for managing wallpapers in awesomewm.
+
+Tapestry allows setting wallpapers for awesome and storing the settings in a
+configuration file. It supports several methods from `gears.wallpaper` and
+setting different wallpapers for different screens.
+
+## Installation
+
+Requirements:
+
+- zsh
+- awesome's dbus interface must be enabled (i.e. `awesome-client` must be
+  working)
+
+Put the contents of this repository inside a folder named `tapestry` in your
+awesome config dir (usually `~/.config/awesome`). If your awesome configuration
+is managed by git, I recommend adding this repo as a git submodule:
+
+```git submodule add https://github.com/crater2150/awesome-tapestry.git tapestry ```
+
+Then, in your `rc.lua`:
+
+```require("tapestry")```
+
+Copy or link `tapestry` to somewhere in your `$PATH`.
+
+## Usage
+
+See `tapestry --help`
+
+TODO: more doc
---
+++ init.lua
@@ -0,0 +1,40 @@
+local beautiful = require("beautiful")
+local gears     = require("gears")
+local awful     = require("awful")
+
+local wallpaperrc = awful.util.getdir("config") .. "/tapestry/wallpaperrc.lua"
+
+local function set_wallpaper(walls, s)
+	local wall= walls[s.index] or walls.default
+	local f = io.open(wall[2], "r")
+	if f~= nil then
+		io.close(f)
+		wall[1](wall[2],s,wall[3],wall[4])
+	end
+end
+
+function update_wallpapers()
+	local f=io.open(wallpaperrc,"r")
+	if f~=nil then
+		io.close(f)
+		walls = dofile(wallpaperrc)
+		for s in screen do
+			set_wallpaper(walls, s)
+		end
+		screen.connect_signal("property::geometry", function(s)
+			set_wallpaper(walls, s)
+		end)
+	elseif beautiful.wallpaper then
+		f = io.open(beautiful.wallpaper)
+		if  f ~= nil then
+			io.close(f)
+			screen.connect_signal("property::geometry", function(s)
+				gears.wallpaper.maximized(beautiful.wallpaper, s, true)
+			end)
+		end
+	end
+end
+
+update_wallpapers()
+
+return update_wallpapers
